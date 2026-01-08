a42894dae00b2fa9ca786aabd3dd903d4e8b3faf
k4rkie <karkeekrish07@gmail.com>
2026-01-08 17:56:35 +0545

    Added time logging and presistent storage of data

--- app_usage_tracker.py
+++ app_usage_tracker.py
@@ -1,34 +1,78 @@
 from subprocess import run
-import time
+import time, datetime
+import signal, sys
+from pathlib import Path
 
-
-def check_if_vscode_active():
+def main():
     print("Press Ctrl+C to exit the program")
-    counter = 0
-    try:    
-        while True:
 
-            # get the active window property
-            x_active_win_props = run(['xprop', '-root', '_NET_ACTIVE_WINDOW'], capture_output=True, text=True).stdout
+    date = datetime.datetime.now()
+    today = date.strftime("%Y-%m-%d")
 
-            # extract the window_id of the active window
-            x_active_win_id = x_active_win_props.split(" ")[4].replace(",","")
+    base_dir = Path.home() / ".local" / "share"
+    app_dir = base_dir / "usage_tracker"
+    app_dir.mkdir(parents=True, exist_ok=True)
+    log_file = app_dir / "app_usage_log.txt"
 
-            # use the active window_id to find the wm_class name of the window
-            x_active_win_class_prop = run(['xprop', '-id', x_active_win_id, 'WM_CLASS'], capture_output=True, text=True).stdout
+    # try creating a new file if doesnt exist
+    try:
+        f = open(log_file, "x")
+        counter = 0
+        f.close()
+    
+    # else read the existing file
+    except FileExistsError:
+        with open(log_file, "r") as f:
+            # check if the exists but is empty
+            if f.read():
+                # reset the file reader pointer to the start"0"
+                f.seek(0)
+                # read the last line of the file
+                read_log_data = f.readlines()[-1]
+                # check if the last line/entry is form today
+                if read_log_data.split(" ")[0].replace(":","") == today:
+                    counter = int(read_log_data.split(" ")[1])
+            else:
+                counter = 0
 
-            # check if the wm_class prop contains the string "code" which is the class name of vs code
-            if x_active_win_class_prop.lower().find("code") != -1 :
-                # if yes add 1sec to the counter
-                counter += 1
-            time.sleep(1)
-            print(x_active_win_class_prop)
+    def signalHandler(signum, frame):
+        print("\nSystem is about to shutdown, Pack up lil bro")
+        with open(log_file, "a") as f:
+            f.write(f"\n{today}: {counter} seconds;")
+        sys.exit(0)
 
-    # Press Ctrl+C to exit the program
-    except KeyboardInterrupt:
-        print(f"\nYou spent a total of {counter} seconds in vscode")
+    # check for any SIGTERM signals for the os and call the handler if TRUE
+    signal.signal(signal.SIGTERM, signalHandler)
 
-check_if_vscode_active()
+    # check for any SIGINT signals for the os and call the handler if TRUE
+    signal.signal(signal.SIGINT, signalHandler)
+    
+    while True:
+
+        # get the active window property
+        x_active_win_props = run(['xprop', '-root', '_NET_ACTIVE_WINDOW'], capture_output=True, text=True).stdout
+
+        # extract the window_id of the active window
+        x_active_win_id = x_active_win_props.split(" ")[4].replace(",","")
+
+        # use the active window_id to find the wm_class name of the window
+        x_active_win_class_prop = run(['xprop', '-id', x_active_win_id, 'WM_CLASS'], capture_output=True, text=True).stdout
+
+        # check if the wm_class prop contains the string "code" which is the class name of vs code
+        if x_active_win_class_prop.lower().find("code") != -1 :
+            # if yes add 1sec to the counter
+            counter += 1
+        time.sleep(1)
+    
+
+
+main()
+
+
+
+
+
+
 
 
 
